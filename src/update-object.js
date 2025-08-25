268e6419711298f5e36592b893f40b1d162d6396
Farhan Maulidian <farhanmaulidian16@gmail.com>
2025-08-25 16:52:45 +0700

    Update script update-object file

--- src/update-object.js
+++ src/update-object.js
@@ -1,13 +1,46 @@
-import http from 'k6/http';
-import { sleep, check } from 'k6';
+import http from "k6/http";
+import { sleep, check, fail } from "k6";
+import { generateRandomInteger } from "./helper/helper";
 
 export const options = {
   vus: 10,
-  duration: '30s',
+  duration: "30s",
 };
 
-export default function() {
-  let res = http.get('https://quickpizza.grafana.com');
-  check(res, { "status is 200": (res) => res.status === 200 });
-  sleep(1);
+export default function () {
+  const randNumb = generateRandomInteger(1, 10);
+
+  const getObjectById = http.get(
+    `https://api.restful-api.dev/objects/${randNumb}`
+  );
+
+  const checkObject = check(getObjectById, {
+    "response status must 200": (response) => response.status === 200,
+  });
+
+  if (!checkObject) {
+    fail();
+  }
+
+  const newData = {
+    name: `Update Data ${new Date().getTime()}`,
+    data: {
+      year: 2019,
+      price: generateRandomInteger(500, 2000),
+      "CPU model": "Intel Core i9",
+      "Hard disk size": "1 TB",
+      color: "silver",
+    },
+  };
+
+  const updateObject = http.post(
+    `https://api.restful-api.dev/objects/${randNumb}`,
+    JSON.stringify(data)
+  );
+
+  check(updateObject, {
+    "response status must 200": (response) => response.status === 200,
+    "updated at field must not null": (response) =>
+      response.json().updated_at != null,
+  });
 }
